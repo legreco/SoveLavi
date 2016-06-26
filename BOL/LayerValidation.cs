6e5242d6b95ca613b8d3df048fba993c3980748b
MarcGrecoPeralte <MarcGrecoPeralte@GRECO>
2016-06-26 10:51:48 -0500

    Add project files.

---
+++ BOL/LayerValidation.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BOL
+{
+   public class LayerValidation
+    {
+
+     
+        public string fileName { get; set; }
+
+        [Display(Name = "Titre")]
+        [Required]
+        public string title { get; set; }
+
+        [Display(Name = "Description")]
+        [Required]
+        public string description { get; set; }
+
+        public System.DateTime dateAdded { get; set; }
+        [Display(Name = "Catégorie")]
+        [Required]
+        public int categoryId { get; set; }
+
+        [Display(Name = "Type")]
+        [Required]
+        public int layerTypeId { get; set; }
+
+      
+        public System.DateTime validFrom { get; set; }
+
+        public System.DateTime validTo { get; set; }
+
+        public string imageName { get; set; }
+    }
+
+
+
+    [MetadataType(typeof(LayerValidation))]
+    public partial class tbl_LAYER
+    {
+
+
+    }
+}
---
+++ SoveLaviUI/Areas/Administrator/Controllers/MapController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SoveLaviUI.Areas.Administrator.Controllers
+{
+    public class MapController : Controller
+    {
+        // GET: Administrator/Map
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
